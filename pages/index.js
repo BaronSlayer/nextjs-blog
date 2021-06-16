8cfa05929faf90433cd5eee1ca0b10badd36a6be
Max Pisaryk <maksim.pisaryk@louisville.edu>
2021-06-16 15:58:30 -0400

    npm i gray-matter; fetching posts data

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,18 @@
 import Head from 'next/head';
 import Link from 'next/link';
 import Layout, { siteTitle } from '../components/Layout';
+import { getSortedPostsData } from '../lib/posts';
 
-export default function Home() {
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData();
+  return {
+    props: {
+      allPostsData,
+    },
+  };
+}
+
+export default function Home({ allPostsData }) {
   return (
     <Layout home>
       <Head>
@@ -17,6 +27,22 @@ export default function Home() {
         </p>
       </section>
 
+      <section className="headingMd padding1px">
+        <h2 className="headingLg">Blog</h2>
+
+        <ul className="list">
+          {allPostsData.map(({ id, date, title }) => (
+            <li className="listItem" key={id}>
+              {title}
+              <br />
+              {id}
+              <br />
+              {date}
+            </li>
+          ))}
+        </ul>
+      </section>
+
       <style jsx>{`
         .container {
           min-height: 100vh;
@@ -27,9 +53,25 @@ export default function Home() {
           align-items: center;
         }
 
+        .headingLg {
+          font-size: 1.5rem;
+          line-height: 1.4;
+          margin: 1rem 0;
+        }
+
         .headingMd {
           font-size: 1.2rem;
           line-height: 1.5;
+        }
+
+        .list {
+          list-style: none;
+          padding: 0;
+          margin: 0;
+        }
+
+        .listItem {
+          margin: 0 0 1.25rem;
         }
       `}</style>
     </Layout>
