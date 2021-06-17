dd9f2592d70040c77b4f84eb2839ec048c67c985
Max Pisaryk <maksim.pisaryk@louisville.edu>
2021-06-17 08:45:42 -0400

    clean up, posts dynamic routes, added remark, remark-html

---
+++ pages/posts/[id].js
@@ -0,0 +1,35 @@
+import Layout from '../../components/Layout';
+import { getAllPostIds, getPostData } from '../../lib/posts';
+
+export async function getStaticProps({ params }) {
+  // Fetch necessary data for the blog post using params.id
+  const postData = getPostData(params.id);
+  return {
+    props: {
+      postData,
+    },
+  };
+}
+
+export async function getStaticPaths() {
+  // Return a list of possible value for id
+  const paths = getAllPostIds();
+  return {
+    paths,
+    fallback: false,
+  };
+}
+
+function Post({ postData }) {
+  return (
+    <Layout>
+      {postData.title}
+      <br />
+      {postData.id}
+      <br />
+      {postData.date}
+    </Layout>
+  );
+}
+
+export default Post;
